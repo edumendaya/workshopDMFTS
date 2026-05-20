310520e11501a7d48e7cb5cfe3e57aeed9577bef
edumendaya <42156868+edumendaya@users.noreply.github.com>
2026-05-20 14:56:03 -0600

    Add files via upload

---
+++ modW2E.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PREFIX="modW2E" # Change the prefix according to the .tpl .est and .obs files
+
+for i in {1..3}
+ do
+   mkdir run$i
+   cp ${PREFIX}.tpl ${PREFIX}.est ${PREFIX}_MSFS.obs fsc28-mac run$i"/"
+   cd run$i
+   ./fsc28-mac -t ${PREFIX}.tpl -e ${PREFIX}.est -m -0 -C 10 -n 20000 -L 40 -s 0 -M -q -c 12 --multiSFS
+   rm -r ${PREFIX}_MSFS.obs
+   cd ..
+ done
+
+
+# Modified from Mark Ravinet & Joana Meier (2021) https://speciationgenomics.github.io/fastsimcoal2
