a9338edcd9e1c8a9657e9adfb0ad2b5de293a30e
edumendaya <42156868+edumendaya@users.noreply.github.com>
2026-05-20 15:33:34 -0600

    Update modW2E.sh

--- modW2E.sh
+++ modW2E.sh
@@ -7,7 +7,7 @@ for i in {1..3}
    mkdir run$i
    cp ${PREFIX}.tpl ${PREFIX}.est ${PREFIX}_MSFS.obs fsc28-mac run$i"/"
    cd run$i
-   ./fsc28-mac -t ${PREFIX}.tpl -e ${PREFIX}.est -m -0 -C 10 -n 20000 -L 30 -s 0 -M -q -c 0 --multiSFS
+   ./fsc28-mac -t ${PREFIX}.tpl -e ${PREFIX}.est -m -0 -C 10 -n 10000 -L 30 -s 0 -M -q -c 0 --multiSFS
    rm -r ${PREFIX}_MSFS.obs
    cd ..
  done
