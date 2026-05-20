26713f1b839c1be181bec6e7420d7a871a9c1075
edumendaya <42156868+edumendaya@users.noreply.github.com>
2026-05-20 15:21:26 -0600

    Update modW2E.sh

--- modW2E.sh
+++ modW2E.sh
@@ -2,12 +2,12 @@
 
 PREFIX="modW2E" # Change the prefix according to the .tpl .est and .obs files
 
-for i in {1..3}
+for i in {2..4}
  do
    mkdir run$i
    cp ${PREFIX}.tpl ${PREFIX}.est ${PREFIX}_MSFS.obs fsc28-mac run$i"/"
    cd run$i
-   ./fsc28-mac -t ${PREFIX}.tpl -e ${PREFIX}.est -m -0 -C 10 -n 20000 -L 40 -s 0 -M -q -c 12 --multiSFS
+   ./fsc28-mac -t ${PREFIX}.tpl -e ${PREFIX}.est -m -0 -C 10 -n 20000 -L 30 -s 0 -M -q -c 0 --multiSFS
    rm -r ${PREFIX}_MSFS.obs
    cd ..
  done
